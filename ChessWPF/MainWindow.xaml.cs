a4ccd9118effccac780feda6d765778fb2b26d9e
FamtanK <98282716+FamtanK@users.noreply.github.com>
2022-06-15 00:28:17 +0300

    added the function of creating a piece

--- ChessWPF/MainWindow.xaml.cs
+++ ChessWPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChessCore;
 
 namespace ChessWPF
 {
@@ -20,9 +21,49 @@ namespace ChessWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Piece> pieces;
         public MainWindow()
         {
             InitializeComponent();
+            pieces = new List<Piece>();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = (Button)sender;
+            var piece = new Piece(1, 1);
+            if (btn.Content == null)
+            {
+                piece = CreatePiece(btn);
+            }
+            else
+            {
+                btn.Background = Brushes.Blue; 
+            }
+        }
+
+        private Piece CreatePiece(Button btn)
+        {
+            var pieceCod = GetPieceCode();
+            var x = Grid.GetRow(btn);
+            var y = Grid.GetColumn(btn);
+            pieces.Add(FigureFab.Make(pieceCod, x, y));
+
+            btn.Content = pieceCod;
+            if (btn.Background == Brushes.Black)
+            {
+                btn.Foreground = Brushes.White;
+            }
+            btn.Content = pieceCod;
+            return pieces.Last();
+        }
+
+        private string GetPieceCode()
+        {
+            var comboBoxItem = (ComboBoxItem)Pieces.SelectedItem;
+            var content = (TextBlock)comboBoxItem.Content;
+            string pieceCode = content.Text;
+            return pieceCode;
         }
     }
 }
